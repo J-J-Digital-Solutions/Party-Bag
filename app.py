27658d28feb169145bd8cfe49f6984f69a945674
jacobhathaway17 <119406903+jacobhathaway17@users.noreply.github.com>
2024-10-28 20:22:25 +0000

    Changes to be committed: 	new file:   app.py 	new file:   static/global.css 	new file:   static/header.css 	new file:   static/homepage.css 	new file:   static/imgs/hero-image.jpg 	new file:   static/product.css 	new file:   templates/base.html 	new file:   templates/home.html 	new file:   templates/product.html

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("home.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
+
+if __name__ == "__main__":
+    app.run()
